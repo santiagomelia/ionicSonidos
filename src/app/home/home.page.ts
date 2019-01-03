2b1e82729f17292e546174101637c9e9162bb611
santiagomelia <santiago.melia@gmail.com>
2019-01-03 13:55:38 +0100

    Añadida la lista

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
 
+import {ANIMALES} from "../data/data.animales"
+
+import {Animal} from "../../interfaces/animal.interface"
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
+
+  animales:Animal[] = [];
+
+
+  constructor(){
+    this.animales = ANIMALES.slice(0); // Esto lo que hace es un clone y así no modifica el origen ANIMALES
+  }
+
+  reproducir (animal: Animal) {
+    console.log (animal);
+
+    let audio = new Audio (); // es un objeto de HTML5
+
+    audio.src = animal.audio;
+
+    audio.load();
+    audio.play();
+
+    animal.reproduciendo = true;
+
+    setTimeout( () => animal.reproduciendo = false, animal.duracion * 1000 )
+  }
 
 }
